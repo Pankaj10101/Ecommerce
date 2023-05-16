4d90215ca705db32db507d178d99df8af293e143
Pankaj10101 <rkay1048@gmail.com>
2023-05-16 14:07:05 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import React, { Suspense } from "react";
 import "./App.css";
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Header/Navbar/Navbar";
 import Cart from "./Components/Cart/Cart";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import About from "./Components/About/About";
-import StoreContainer from "./Components/Store/Store";
+// import StoreContainer from "./Components/Store/Store";
 import Home from "./Components/Home/Home";
 import ContactUs from "./Components/ContactUs/ContactUs";
-import ProductPage from "./Components/Store/ProductPage";
+// import ProductPage from "./Components/Store/ProductPage";
 import Login from "./Components/Login/Login";
 import { useContext } from "react";
 import { Store } from "./store/context";
+
+const StoreContainer = React.lazy(() => import("./Components/Store/Store"));
+const ProductPage = React.lazy(() => import("./Components/Store/ProductPage"));
 
 function App() {
   const { loggedIn } = useContext(Store);
@@ -18,24 +22,27 @@ function App() {
     <>
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-          {loggedIn && (
-            <>
-              <Route path="/store" element={<StoreContainer />} />
-              <Route path="/store/:id" element={<ProductPage />} />
-              <Route path="/cart" element={<Cart />} />{" "}
-            </>
-          )}
-          <Route path="*" element={<Navigate to="/login" />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+            {loggedIn && (
+              <>
+                <Route path="/store" element={<StoreContainer />} />
+                <Route path="/store/:id" element={<ProductPage />} />
+                <Route path="/cart" element={<Cart />} />{" "}
+              </>
+            )}
+            <Route path="*" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </BrowserRouter>
     </>
   );
 }
+
 
 export default App;
